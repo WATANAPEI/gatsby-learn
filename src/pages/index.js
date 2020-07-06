83aaba5e6b92c91f097b5748241dad9f06b39676
WATANAPEI <info@wpei.dev>
2020-07-06 23:52:27 +0900

    add seo setting

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,12 @@ import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 
+import SEO from "../components/seo"
+
 export default function Home({ data }) {
   return (
     <Layout>
+      <SEO />
       <section className="hero">
         <figure>
           <Img fluid={data.hero.childImageSharp.fluid}
