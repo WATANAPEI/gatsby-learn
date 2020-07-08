fa6e71996ac5b6da9a9191ed4f1ad47c72bafdec
WATANAPEI <info@wpei.dev>
2020-07-09 00:01:39 +0900

    added open graph image

--- src/components/seo.js
+++ src/components/seo.js
@@ -23,6 +23,12 @@ export default props => {
     const url = props.pagepath
         ? `${data.site.siteMetadata.siteUrl}${props.pagepath}`
         : data.site.siteMetadata.siteUrl
+    const imgurl = props.pageimg
+        ? `${data.site.siteMetadata.siteUrl}${props.pageimg}`
+        : `${data.site.siteMetadata.siteUrl}/thumb.jpg`
+
+    const imgw = props.pageimgw || 1280
+    const imgh = props.pageimgh || 640
 
     return (
         <Helmet>
@@ -30,6 +36,11 @@ export default props => {
             <title>{title}</title>
             <meta name="description" content={description} />
             <link rel="canonical" href={url} />
+            <meta property="og:image" content={imgurl} />
+            <meta property="og:image:width" content={imgw} />
+            <meta property="og:image:height" content={imgh} />
+
+            <meta name="twitter:card" content="summary_large_image" />
         </Helmet>
     )
 
